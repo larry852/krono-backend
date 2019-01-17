cf2e15f4dd02a36b862a54f0a14c4e9333684384
larry852 <larryportocarrero@gmail.com>
2019-01-17 17:10:39 -0500

    Serialization users - str cities

--- cities/models.py
+++ cities/models.py
@@ -6,3 +6,6 @@ User = get_user_model()
 
 class City(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
--- stores/serializers.py
+++ stores/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Store
+from users.serializers import UserSerializer
 
 
 class StoreSerializer(serializers.ModelSerializer):
+    users = UserSerializer(read_only=True, many=True)
+
     class Meta:
         model = Store
         fields = ('__all__')
+        extra_kwargs = {
+            'is_active': {'read_only': True},
+        }
