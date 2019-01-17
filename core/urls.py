b4764b55bff8ad4ae60a6a5f3495ab491e7b4f4d
larry852 <larryportocarrero@gmail.com>
2019-01-17 11:33:00 -0500

    Add documentation API

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
 from users.urls import router as users_router
+from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
 router.registry.extend(users_router.registry)
@@ -11,6 +12,7 @@ router.registry.extend(users_router.registry)
 urlpatterns = [
     path('', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('api/v1/docs/', include_docs_urls(title='Krono Backend'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
