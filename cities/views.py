eb90cb3d16de960ff88523c44fc4468f8727cc1f
larry852 <larryportocarrero@gmail.com>
2019-01-17 12:33:14 -0500

    Add cities views

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CitySerializer
+from .models import City
 
-# Create your views here.
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
