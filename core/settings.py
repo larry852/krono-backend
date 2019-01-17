b0e19979c504a86662789a9b46b7d1dcb4994b98
larry852 <larryportocarrero@gmail.com>
2019-01-17 12:28:01 -0500

    Add cities model

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'stores.apps.StoresConfig',
+    'cities.apps.CitiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
