50a5c5e4e437fff181ec0ff4b781f8a94f1e1377
larry852 <larryportocarrero@gmail.com>
2019-01-17 10:04:22 -0500

    Add users view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_staff=False)
+    serializer_class = UserSerializer
