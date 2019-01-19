1dcd57df6e6e5ad9f0ab1ad1f770f0953f294319
larry852 <larryportocarrero@gmail.com>
2019-01-18 23:33:36 -0500

    Get city stores

--- cities/views.py
+++ cities/views.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
 from .serializers import CitySerializer
 from .models import City
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from stores.serializers import StoreSerializer
+from stores.models import Store
 
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+    @detail_route(methods=['get'])
+    def stores(self, request, pk=None):
+        city = self.get_object()
+        stores = Store.objects.filter(city=city)
+        serializer = StoreSerializer(stores, many=True)
+        return Response(serializer.data)
