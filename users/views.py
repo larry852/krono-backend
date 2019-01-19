91dc1a0eb177dc3cbd1d64494573e28eda421e34
larry852 <larryportocarrero@gmail.com>
2019-01-19 00:15:54 -0500

    Get user stores

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
-
+from rest_framework.decorators import action
+from stores.serializers import StoreSerializer
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -9,3 +11,10 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(is_staff=False)
     serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'])
+    def stores(self, request, pk=None):
+        user = self.get_object()
+        stores = user.store_set.all()
+        serializer = StoreSerializer(stores, many=True)
+        return Response(serializer.data)
